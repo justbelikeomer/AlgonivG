7fa488a10222e4e3c7fc09c5cbd0107a6d0dd5c9
Omer Farooq <omerfarooq3948@gmail.com>
2025-02-21 20:11:09 +0500

    Delete ai4free-wrapper-main directory

--- ai4free-wrapper-main/run.py
+++
@@ -1,9 +0,0 @@
-# run.py
-
-from app import create_app
-from app.config import Config  # Import the Config class
-
-app = create_app(Config)  # Use the Config class
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
